68630d76cdefd86c230966d94a5967093fc5e380
jinlukang1 <31444152+jinlukang1@users.noreply.github.com>
2019-03-29 20:51:19 +0800

    整数因子分解问题
    如12=3*3*2，12=6*2，计算n共有多少种不同的分解式

--- main.cpp
+++ main.cpp
@@ -4,83 +4,22 @@ using namespace std;
 
 ifstream fin("input.txt");
 ofstream fout("output.txt");
+int count = 0;
 
-double mini(int n, double *x){//求最小值 
-	double min = x[0];
-	for(int i = 1; i < n; i++)
+double solve(int n){//递归的进行 
+	count++;//至少有一个 
+	for(int i = 2; i <= n/2; i++)
 	{
-		if(min > x[i]) min = x[i];
+		if(n%i==0) solve(i);// i -> n/i也是可以的 
 	}
-	return min;
-}
-double maxi(int n, double *x){//求最大值 
-	double max = x[0];
-	for(int i = 1; i < n; i++)
-	{
-		if(max < x[i]) max = x[i];
-	}
-	return max;
 }
 
 int main()
 {
 	int n = 0;
 	fin >> n;
-	double *x = new double[n];//注意fin>>n要写在申请动态数组前面 
-
-
-//	cout << n; 
-	for(int i = 0; i < n; i++) 
-	{
-	fin >> x[i];
-//	cout << x[i] << " ";
-	}
-	double max_all = 0, min_all = 0;
-	
-
-	
-	max_all = maxi(n, x);
-	min_all = mini(n, x);
-	
-//	cout << max_all << " ";
-//	cout << min_all << " "; 
-	
-	int *count = new int[n]; 
-	double *low = new double[n];
-	double *high = new double[n];
-	
-	for(int i = 0; i < n; i++)
-	{
-		count[i] = 0;
-		low[i] = max_all;
-		high[i] = min_all;
-	}
-
-	
-	
-	double bar_len = (max_all-min_all)/(n-1);//计算子区间长度(注意类型定义要为double，不能为整形） 
-	for(int i = 0; i < n; i++)
-	{
-		int index = (int)((x[i]-min_all)/bar_len);
-//		cout << index << " ";
-		count[index]++;
-		if(x[i]<low[index]) low[index] = x[i];
-		if(x[i]>high[index]) high[index] = x[i];
-	}//将n-2个数字放入n-1个桶中
-	
-//	for(int i=0; i < n; i++) cout << endl << count[i] << " " << low[i] << " " << high[i] << endl;
-	
-	double temp = 0, max_distance = 0,left = high[0];
-	for(int i = 1; i < n-1; i++)
-	{
-		if(count[i])
-		{
-			temp = low[i] - left;
-			if(max_distance < temp) max_distance = temp;
-			left = high[i];
-		}
-	 }
-	fout << max_distance;
+	solve(n);
+	fout << count;
 
 	return 0;
 } 
